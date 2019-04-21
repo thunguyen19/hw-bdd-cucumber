7f770630554ed413ff23c10db601f0794b546cf2
tnguyen <thu.nguyen@vudu.com>
2019-04-21 15:37:32 -0700

    add filter movie list feature

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -24,10 +24,21 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
+
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G, NC-17, PG-13
+
   # enter step to "submit" the search form on the homepage
+  And I press "ratings_submit"
+
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see all movies with ratings: PG, R
+
   # enter step(s) to ensure that other movies are not visible
+  And I should not see all movies with ratings: G, NC-17, PG-13
 
 Scenario: all ratings selected
   # see assignment
+  When I check all ratings
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -29,10 +29,37 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rates = rating_list.split(", ")
+  if uncheck.nil?
+    rates.each{ |r| steps %Q{When I check "ratings_#{r}"}}
+  else
+    rates.each{ |r| steps %Q{When I uncheck "ratings_#{r}"}}
+  end
+end
+
+When /I check all ratings/ do
+  rates = Movie.all_ratings
+  rates.each{ |r| steps %Q{When I check "ratings_#{r}"}}
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('table#movies tr').count - 1 
+  expect(rows).to eq(Movie.count)
+end
+
+Then /I should see all movies with ratings: (.*)/ do |rating_list|
+  # Make sure that all the movies in the app are visible in the table
+  rates = rating_list.split(", ")
+  movie_list = []
+  rates.each{|r| movie_list = movie_list + Movie.where(rating: r)}
+  movie_list.each{|movie| steps %Q{Then I should see "#{movie[:title]}"}} 
+end
+
+Then /I should not see all movies with ratings: (.*)/ do |rating_list|
+  # Make sure that all the movies in the app are visible in the table
+  rates = rating_list.split(", ")
+  movie_list = []
+  rates.each{|r| movie_list = movie_list + Movie.where(rating: r)}
+  movie_list.each{|movie| steps %Q{Then I should not see "#{movie[:title]}"}} 
 end
